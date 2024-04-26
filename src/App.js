c57460802a20ec83ac2cb6b1e0d5ae9ae05fddd5
arnabjoy123 <arnab99joy@gmail.com>
2024-04-26 18:30:29 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/cart";
+import Home from "./pages/home";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
